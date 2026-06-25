a4ccf812674139cb8deafe1f988d39210adad3cb
nebu <126800543+nebvx@users.noreply.github.com>
2026-06-25 18:46:13 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,51 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
+std::vector<std::string> getVectorOfInput(std::string user_input) {
+  std::cout << "getvector called \n";
+
+  std::vector<std::string> input; //the whole input fromm the user sperated by spaces
+  std::string command = ""; //can be echo or the words after
+
+  for (size_t i {0}; i < user_input.size(); ++i) {
+    if (user_input.at(i) == ' ') {
+      input.push_back(command);
+      std::cout << "command: " << command << "\n"; //debugging
+      command = "";
+    } else {
+      command += user_input[i];
+    }
+  }
+
+  return input;
+}
+
+void handleEcho(std::vector<std::string> input) {
+  std::cout << "handleecho called \n";
+  
+  for (auto word : input) {
+    if (word != "echo"){
+      std::cout << word;
+    }
+  }
+
+}
+
+
+bool handleInput(std::string user_input) {
+  std::cout << "handleinputcalled \n";
+  std::vector<std::string> input = getVectorOfInput(user_input);
+  if (input.at(0) == "echo") {
+    handleEcho(input);
+  } else if (input.at(0) == "exit") {
+    return false;
+  } else {
+    std::cout << user_input << ": command not found \n";
+  }
+  return true;
+}
+
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -10,9 +56,13 @@ int main() {
   std::string user_input = "";
   while(true) {
     std::cout << "$ ";
+    //user input
     std::cin >> user_input;
-    if (user_input == "exit") break;
-    std::cout << user_input << ": command not found \n";
+    if (!handleInput(user_input)) {
+      std::cout << "exit called \n"; //debug
+      break;
+    }
+
     user_input = "";
   }
 }
