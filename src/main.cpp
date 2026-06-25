65770c147c4f22686ea58ea4b785b871f81395b5
nebu <126800543+nebvx@users.noreply.github.com>
2026-06-26 00:04:26 +0200

    add echo function [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -3,38 +3,33 @@
 #include <vector>
 
 std::vector<std::string> getVectorOfInput(std::string user_input) {
-  //std::cout << "getvector called \n";
-
   std::vector<std::string> input; //the whole input fromm the user sperated by spaces
   std::string command = ""; //can be echo or the words after
 
   for (size_t i {0}; i < user_input.size(); ++i) {
     if (user_input.at(i) == ' ') {
       input.push_back(command);
-      std::cout << "command: " << command << "\n"; //debugging
       command = "";
     } else {
       command += user_input[i];
     }
   }
-
+  
+  input.push_back(command);
   return input;
 }
 
 void handleEcho(std::vector<std::string> input) {
- // std::cout << "handleecho called \n";
-  
   for (auto word : input) {
     if (word != "echo"){
-      std::cout << word;
+      std::cout << word << " ";
     }
   }
+  std::cout << "\n";
 
 }
 
-
 bool handleInput(std::string user_input) {
-  //std::cout << "handleinputcalled \n";
   std::vector<std::string> input = getVectorOfInput(user_input);
   if (input.at(0) == "echo") {
     handleEcho(input);
@@ -57,12 +52,14 @@ int main() {
   while(true) {
     std::cout << "$ ";
     //user input
-    std::cin >> user_input;
-    if (!handleInput(user_input)) {
-      //std::cout << "exit called \n"; //debug
-      break;
+    std::getline(std::cin, user_input);
+    if (!user_input.empty()) {
+      if (!handleInput(user_input)) {
+        //std::cout << "exit called \n"; //debug
+        break;
+      }
     }
-
+  
     user_input = "";
   }
 }
